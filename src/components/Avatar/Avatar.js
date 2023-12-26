13accbf6d32cc9cf50578a6cd909edd6167147e8
naveenmathramkott <naveenmathramkott@gmail.com>
2023-12-26 19:11:56 +0400

    cleared files

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -5,18 +5,22 @@ import AvatarImg from "../../assets/images/avatar.jpg";
 const Avatar = () => {
   const classes = useStyles();
   return (
-      <img src={AvatarImg} alt="Mehdi BHA" className={classes.avatarImg} />
+    <img
+      src={AvatarImg}
+      alt="Naveen Mathramkott"
+      className={classes.avatarImg}
+    />
   );
 };
 
 const useStyles = makeStyles((theme) => ({
   avatarImg: {
-    borderRadius:'50%',
-    width:"270px",
-    height:"270px",
-    objectFit:'cover',
+    borderRadius: "50%",
+    width: "270px",
+    height: "270px",
+    objectFit: "cover",
     objectPosition: "0 -20px",
-    boxShadow:theme.shadows[10]
+    boxShadow: theme.shadows[10],
   },
 }));
 
